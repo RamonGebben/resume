032be61d26ef4afbeba5abaa73f17cc26865c8f9
Ramon Gebben <ramon@ra-ge.net>
2015-12-08 19:52:38 +0100

    Init

---
+++ resume.js
@@ -0,0 +1,149 @@
+function Resume(){
+
+}
+
+Resume.prototype.drawResume = function(){
+
+}
+
+
+var ramon = {
+  "basics": {
+    "name": "Ramon Gebben",
+    "label": "Developer",
+    "picture": "https://lh6.googleusercontent.com/-Z7bkC9IUDzY/AAAAAAAAAAI/AAAAAAAADxI/WdXiSbowOGo/s120-c/photo.jpg",
+    "email": "ramon@ra-ge.net",
+    "phone": "+316 30 97 85 40",
+    "website": "http://ra-ge.net",
+    "summary": "I am a 23 year old frontend developer using HTML(5), CSS(3)/SCSS/LESS, Javascript(jQuery, Coffee and Vanilla) and Ruby on Rails or Nodejs as backend. I love working on projects such as presentation tools, process tools and complete websites with custom CMS. I looking to work for a super awesome company where I can develop into the role of fontend engineer and designer. I would love to learn more about tools such as databinding systems and frontend frameworks and further extend my knowledge on general web development.",
+    "location": {
+      "address": "Plutolaan 158",
+      "postalCode": "3721 MV",
+      "city": "Bilthoven",
+      "countryCode": "NL",
+      "region": "Utrecht"
+    },
+    "profiles": [
+      {
+        "network": "Twitter",
+        "username": "Ra_Ge_NET",
+        "url": "https://twitter.com/Ra_Ge_NET"
+      }
+    ]
+  },
+  "work": [
+    {
+      "company": "Qelp",
+      "position": "Frontend Developer",
+      "website": "http://qelp.com",
+      "startDate": "2014-08-06",
+      "endDate": "",
+      "summary": "Qelp is a company that provides smartphone help to non technical people. With QelpCare you can offer your customers 24/7 support through the channel of their liking and availability: online, mobile, retail, call center and even if they search for help using Google. Picture based tutorials, one-click fix and remote diagnostics are available to resolve complex issues or educate about new functions to increase your revenues per user.",
+      "highlights": [
+        "Developed a device emulator for the webbrowser. To mimic every phone."
+      ]
+    },
+    {
+      "company": "Various Kitchens",
+      "position": "Chef",
+      "website": "",
+      "startDate": "2012",
+      "endDate": "2013",
+      "summary": "After my training as a chef I quickly fought my way up the ladder to become a sous-chef( second man ) and started to lead a team of 3.",
+      "highlights": [
+        "Introduced serveral new dishes by various kitchens."
+      ]
+    }
+  ],
+  "education": [
+    {
+      "institution": "ICT Acedemy Utrecht",
+      "area": "System Administration",
+      "studyType": "MBO",
+      "startDate": "2013-01-01",
+      "endDate": "2014-01-01",
+      "courses": [
+        "Networking",
+        "Webhosts",
+        "ActiveDirectory",
+        "Linux"
+      ]
+    }
+  ],
+  "volunteer": [
+    {
+      "organization": "Saterday Morning Coding Utrecht",
+      "position": "Coörganizer",
+      "website": "http://www.meetup.com/Saturday-Morning-Coding-Utrecht/",
+      "startDate": "2013-05-05",
+      "endDate": "",
+      "summary": "This is a small community I joined about a year ago. It's not more then a bunch of nice people with a passion for code. We get together every Saturday morning to drink coffee and work on projects, together or on your own project anything goes.",
+      "highlights": [
+        "Won the 2014 Green Hackfest Utrecht."
+      ]
+    }
+  ],
+  "awards": [
+    {
+      "title": "First place visualising sustainability",
+      "date": "2014-10-12",
+      "awarder": "Green Hackfest Utrecht",
+      "summary": "For this event me and my team made an better solution for using electronic car charging stations. This solution was taking a lot of measurements such as CO2 particles, heat and charging levels of the car. With these measurements we could calculate when a car could be moved and send notifications the the owner of the car to move it.   "
+    }
+  ],
+  "publications": [],
+  "skills": [
+    {
+      "name": "Web Development",
+      "level": "Master",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "LESS",
+        "SCSS",
+        "Javascript",
+        "Node",
+        "Ruby"
+      ]
+    },
+    {
+      "name": "Frameworks",
+      "level": "Master",
+      "keywords": [
+        "Rails",
+        "Sinatra",
+        "Express",
+        "Sails",
+        "EmberJS"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "language": "English",
+      "fluency": "Fluent"
+    },
+    {
+      "language": "Dutch",
+      "fluency": "Native speaker"
+    }
+  ],
+  "interests": [
+    {
+      "name": "Pet projets",
+      "keywords": [
+        "Web apps",
+        "CLI's"
+      ]
+    },
+    {
+      "name": "Music",
+      "keywords": [
+        "Dubstep",
+        "Drum'n'bass",
+        "Rock"
+      ]
+    }
+  ],
+  "references": []
+};
